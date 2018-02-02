bac8b402e5021fa499f3d7ee9806b66fe7e85fde
Bluefire2 <chernyshov.k@gmail.com>
2018-02-01 22:33:02 -0500

    Better way to set initial state

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const initialState = {
+    fieldSquares: [[0, 1, 2], [2, 3, 4], [0, 2, 4]]
+};
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={createStoreWithMiddleware(reducers, initialState)}>
         <App/>
     </Provider>
     , document.getElementById('root'));
--- src/reducers/field_squares_reducer.js
+++ src/reducers/field_squares_reducer.js
@@ -2,6 +2,6 @@ export default (state = null, action) => {
     switch (action.type) {
         default:
             // feed in a test input
-            return [[0, 1, 2], [2, 3, 4], [0, 2, 4]];
+            return state;
     }
 }
