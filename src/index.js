6714f9941d1ac90592510cb3489b22b387286b82
Bluefire2 <chernyshov.k@gmail.com>
2018-02-02 11:40:34 -0500

    Add moves state field, and implement its reducer
    Moves start at 0, and increase by 1 after each change of colour.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const initialState = {
-    fieldSquares: [[0, 0, 2], [0, 3, 4], [0, 2, 4]]
+    fieldSquares: [[0, 0, 2], [0, 3, 4], [0, 2, 4]],
+    moves: 0
 };
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
 import fieldSquaresReducer from './field_squares_reducer';
+import movesReducer from './moves_reducer';
 
 const rootReducer = combineReducers({
-    fieldSquares: fieldSquaresReducer
+    fieldSquares: fieldSquaresReducer,
+    moves: movesReducer
 });
 
 export default rootReducer;
